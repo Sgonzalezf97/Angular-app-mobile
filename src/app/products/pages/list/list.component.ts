ea7418325d9a07c400ea6e5cbe188c521c33356c
Sebastian Gonzalez <jusegofo@gmail.com>
2024-02-10 00:01:52 -0500

    search Input implemented

--- src/app/products/pages/list/list.component.ts
+++ src/app/products/pages/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Product } from '../../interfaces/product.interface';
 import { ProductsService } from '../../services/product.service';
 
@@ -10,6 +12,9 @@ import { ProductsService } from '../../services/product.service';
 export class ListComponent implements OnInit{
 
   public productos: Product[] = [];
+  public searchInput= new FormControl('');
+  public products: Product[]=[];
+  public selectedHero?: Product;
 
   constructor(private productsService: ProductsService){
 
@@ -17,5 +22,22 @@ export class ListComponent implements OnInit{
   ngOnInit(): void {
     this.productsService.getProducts()
     .subscribe(productos => this.productos = productos);
+  }
+
+  searchProduct(){
+    const value: string = this.searchInput.value || '';
+    this.productsService.getSuggestions(value)
+    .subscribe(products => this.products = products);
+  }
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent):void{
+    if (!event.option.value) {
+      this.selectedHero=undefined;
+      return;
+    }
+
+    const product: Product = event.option.value;
+    this.searchInput.setValue( product.nombre);
+    this.selectedHero=product;
   }
 }
--- src/app/products/pages/search-page/search-page.component.ts
+++ src/app/products/pages/search-page/search-page.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Product } from '../../interfaces/product.interface';
+import { ProductsService } from '../../services/product.service';
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styles: ``
 })
 export class SearchPageComponent {
+  public searchInput= new FormControl('');
+  public products: Product[]=[];
+  public selectedHero?: Product;
 
+  constructor(private productsService: ProductsService){}
+
+  searchProduct(){
+    const value: string = this.searchInput.value || '';
+    this.productsService.getSuggestions(value)
+    .subscribe(products => this.products = products);
+  }
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent):void{
+    if (!event.option.value) {
+      this.selectedHero=undefined;
+      return;
+    }
+
+    const product: Product = event.option.value;
+    this.searchInput.setValue( product.nombre);
+    this.selectedHero=product;
+  }
 }
